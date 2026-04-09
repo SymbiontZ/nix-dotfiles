de702fc6e3db4bb4602e53d4a3b247fc23f61114
FabioR <fabiorrojas56@gmail.com>
2026-04-10 00:29:01 +0200

    Agregar comentarios para la configuración de Yuzu y su repositorio de plugins

--- home/appconfs/programs.nix
+++ home/appconfs/programs.nix
@@ -45,4 +45,31 @@
             shuffle
         ];
     };
+
+    # # Yuzu con plugins personalizados
+    # programs.yuzu = {
+    #     enable = true;
+    # };
+
+    # # Clonar repositorio de plugins de Yuzu
+    # home.activation = {
+    #     cloneYuzuPlugins = pkgs.lib.hm.dag.entryAfter ["writeBoundary"] ''
+    #         $DRY_RUN_CMD mkdir -p $HOME/.local/share/yuzu/plugins
+            
+    #         YUZU_PLUGINS_DIR="$HOME/.local/share/yuzu/plugins"
+    #         YUZU_REPO_DIR="$YUZU_PLUGINS_DIR/mi-config"
+            
+    #         if [ ! -d "$YUZU_REPO_DIR" ]; then
+    #             echo "Clonando repositorio de plugins de Yuzu..."
+    #             $DRY_RUN_CMD ${pkgs.git}/bin/git clone \
+    #                 --branch tu-rama-aqui \
+    #                 https://github.com/tu-usuario/tu-repo-yuzu-plugins.git \
+    #                 "$YUZU_REPO_DIR"
+    #         else
+    #             echo "Actualizando repositorio de plugins de Yuzu..."
+    #             cd "$YUZU_REPO_DIR"
+    #             $DRY_RUN_CMD ${pkgs.git}/bin/git pull origin tu-rama-aqui
+    #         fi
+    #     '';
+    # };
 }
